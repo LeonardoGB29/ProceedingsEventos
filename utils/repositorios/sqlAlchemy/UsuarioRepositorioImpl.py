9caa9c0574cb06b482a894b78c5d3c35254c5b8c
DanielfQo <danielfrank12122004@gmail.com>
2024-07-29 14:05:35 -0500

    agregando verificacion de usuarios repetidos

--- utils/repositorios/sqlAlchemy/UsuarioRepositorioImpl.py
+++ utils/repositorios/sqlAlchemy/UsuarioRepositorioImpl.py
@@ -2,11 +2,13 @@
 #-*- coding: utf-8 -*-
 
 from utils.repositorios.sqlAlchemy.conexionBd import db
+from models.entidades.Usuario import Usuario
 
 def agregar_usuario_bd(usuario):
     db.session.add(usuario)
     db.session.commit()
 
+def verificar_usuario_bd(usuario):
+    usuario_db = db.session.query(Usuario).filter_by(nombres=usuario.nombres, apellidos=usuario.apellidos, email=usuario.email).first()
+    return usuario_db is None
 
-class UsuarioRepositorioImpl():
-    pass
--- utils/servicios/ServicioUsuario.py
+++ utils/servicios/ServicioUsuario.py
@@ -1,7 +1,12 @@
-from utils.repositorios.sqlAlchemy.UsuarioRepositorioImpl import agregar_usuario_bd
+from utils.repositorios.sqlAlchemy.UsuarioRepositorioImpl import agregar_usuario_bd, verificar_usuario_bd
 from utils.repositorios.sqlAlchemy.conexionBd import db
 
 def registrar_usuario(usuario):
+    
+    if not verificar_usuario_bd(usuario):
+        return False
+    
     agregar_usuario_bd(usuario)
+    return True
 
-    return "guardando usuario"
+    
