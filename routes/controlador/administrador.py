b1aa4785d153cefa129963c32349edb8e55f089f
LeonardoGB29 <lgaonab@unsa.edu.pe>
2024-07-30 20:10:59 -0500

    arreglando controladores

--- routes/controlador/administrador.py
+++ routes/controlador/administrador.py
@@ -4,18 +4,20 @@ from utils.repositorios.sqlAlchemy.conexionBd import db
 
 administrador = Blueprint('administrador', __name__, template_folder='../templates/vista/HTML')
 
-@administrador.route('/crear_evento', methods=['GET', 'POST'])
-def crear_evento():
-    if request.method == 'POST':
-        nombre = request.form['nombre']
-        fecha = request.form['fecha']
-        edicion = request.form['edicion']
-        nuevo_evento = Evento(nombre, fecha, edicion)
-        db.session.add(nuevo_evento)
-        db.session.commit()
-        flash('Evento creado exitosamente', 'success')
-        return redirect(url_for('administrador.crear_evento'))
+@administrador.route('/crear_evento', methods=['GET'])
+def crear_evento_form():
     return render_template('vista/assets/HTML/crear_evento.html')
+
+@administrador.route('/crear_evento', methods=['POST'])
+def crear_evento():
+    nombre = request.form['nombre']
+    fecha = request.form['fecha']
+    edicion = request.form['edicion']
+    nuevo_evento = Evento(nombre, fecha, edicion)
+    db.session.add(nuevo_evento)
+    db.session.commit()
+    flash('Evento creado exitosamente', 'success')
+    return redirect(url_for('administrador.crear_evento_form'))
 
 
 #@administrador.route('/actua_evento', methods=['GET', 'POST'])
--- routes/controlador/autor.py
+++ routes/controlador/autor.py
@@ -6,18 +6,20 @@ from utils.repositorios.sqlAlchemy.conexionBd import db
 
 autor = Blueprint('autor', __name__, template_folder='../templates/vista/HTML')
 
-@autor.route('/subir_documento', methods=['GET', 'POST'])
-def subir_documento():
-    if request.method == 'POST':
-        resumen = request.form['resumen']
-        datos = request.form['datos']
-        conclusion = request.form['conclusion']
-        num_pag = request.form['num_pag']
-        evento_id = request.form['evento_id']
-        nuevo_documento = Documento(resumen, datos, conclusion, num_pag, evento_id)
-        db.session.add(nuevo_documento)
-        db.session.commit()
-        flash('Documento subido exitosamente', 'success')
-        return redirect(url_for('autor.subir_documento'))
+@autor.route('/subir_documento', methods=['GET'])
+def subir_documento_form():
     eventos = Evento.query.all()
     return render_template('vista/assets/HTML/subir_documento.html', eventos=eventos)
+
+@autor.route('/subir_documento', methods=['POST'])
+def subir_documento():
+    resumen = request.form['resumen']
+    datos = request.form['datos']
+    conclusion = request.form['conclusion']
+    num_pag = request.form['num_pag']
+    evento_id = request.form['evento_id']
+    nuevo_documento = Documento(resumen, datos, conclusion, num_pag, evento_id)
+    db.session.add(nuevo_documento)
+    db.session.commit()
+    flash('Documento subido exitosamente', 'success')
+    return redirect(url_for('autor.subir_documento_form'))
